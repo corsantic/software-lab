6f5b2e01a889a8f8077a311e57aba2ea43a73478
Ferdi Elik <elik.ferdi@gmail.com>
2017-03-03 12:33:49 +0300

    gitignore update

---
+++ software-lab-II-I/src/main/java/QLearningInitializer.java
@@ -0,0 +1,12 @@
+public class QLearningInitializer
+{
+    public static void main(String[] args)
+    {
+        new QLearningInitializer().run(args);
+    }
+
+    void run(String[] args)
+    {
+        System.out.println("......");
+    }
+}
