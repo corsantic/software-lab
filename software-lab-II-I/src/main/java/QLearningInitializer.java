de39beff3ce963be3f464fccec7986f669963745
Enemo <furkan_ay96@hotmail.com>
2017-03-08 02:07:15 +0300

    .

--- software-lab-II-I/src/main/java/QLearningInitializer.java
+++ software-lab-II-I/src/main/java/QLearningInitializer.java
@@ -17,19 +17,28 @@ public class QLearningInitializer
 //        String out=input.nextLine();
 //        System.out.print("target room:");
 //        String in=input.nextLine();
-        new QLearningInitializer().run(args);
+     //   new QLearningInitializer().run(args);
+
+        int n;
 
 
         File file = new File("document/software-lab-II-1/inputs/input22.txt");
         BufferedReader reader = null;
         reader = new BufferedReader(new FileReader(file));
-        String satir = reader.readLine();
 
-        while (satir != null) {
-            System.out.println(satir);
-            satir = reader.readLine();
+        String line = reader.readLine();
+
+        while (line != null) {
+            System.out.println(line);
+            line = reader.readLine();
+
 
     }
+    n=line.length();
+        int i=0,j=0;
+        int[][] R=new int[n][n];
+
+
 
 
 
