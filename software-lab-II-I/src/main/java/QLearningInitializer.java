7f233f47632ae96c7083bc919fdb046d98470dda
Enemo <furkan_ay96@hotmail.com>
2017-03-10 18:59:49 +0300

    .

--- software-lab-II-I/src/main/java/QLearningInitializer.java
+++ software-lab-II-I/src/main/java/QLearningInitializer.java
@@ -1,55 +1,57 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 
-public class QLearningInitializer
-{
+import java.util.Scanner;
+
+public class QLearningInitializer {
 
 
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws IOException {
+
+        new QLearningInitializer().run(args);
 
 
-//        Scanner input=new Scanner(System.in);
+    }
+
+    void run(String[] args) {
+        System.out.println("......");
+        System.out.print("Matris boyutu:");
+        Scanner input = new Scanner(System.in);
 //        System.out.print("beginning room: ");
 //
 //        String out=input.nextLine();
 //        System.out.print("target room:");
 //        String in=input.nextLine();
-     //   new QLearningInitializer().run(args);
-
-        int n;
 
 
-        File file = new File("document/software-lab-II-1/inputs/input22.txt");
-        BufferedReader reader = null;
-        reader = new BufferedReader(new FileReader(file));
+        int n = input.nextInt();
+        int[][] R = new int[n][n];
 
-        String line = reader.readLine();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                R[i][j] = -1;
 
-        while (line != null) {
-            System.out.println(line);
-            line = reader.readLine();
+//                for (int ar = 0; ar <= n; ar++) {
+//                        if ( j== neighbor[ar]) {
+//                            R[i][j] = 0;
+//                        }
 
 
+            }
+
+        }
+        System.out.print("R matrix:");
+        for (int i = 0; i < n; i++)
+
+        {
+            System.out.println("");
+            for (int j = 0; j < n; j++) {
+                System.out.print(R[i][j] + " ");
+
+            }
+
+
+        }
     }
-    n=line.length();
-        int i=0,j=0;
-        int[][] R=new int[n][n];
-
-
-
-
-
-
-    }
-
-    void run(String[] args)
-    {
-        System.out.println("......");
-    }
-
 
 
 }
