8b2e937c27d43e87bfee877ee5671c7e772a5ef6
Ferdi Elik <elik.ferdi@gmail.com>
2017-03-21 09:34:55 +0300

    aded software lab 2 document

---
+++ software-lab-II-II/src/main/java/Initializer.java
@@ -0,0 +1,13 @@
+public class Initializer
+{
+    public static void main(String[] args)
+    {
+        new Initializer().run();
+    }
+
+    void run()
+    {
+        System.out.println("gene mi c4.5? ");
+    }
+}
+
