ffcfa6512c6f27cfdac9f44c241a6219154b674b
Enemo <furkan_ay96@hotmail.com>
2017-03-25 21:11:38 +0300

    update

---
+++ software-lab-II-II/src/main/java/Thread_Write.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * Thread Example multi
+ */
+public class Thread_Write extends Thread {
+    String text = "Thread example-1";
+
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                System.out.println(text);
+                Thread.sleep(5000);
+            }
+            catch (InterruptedException Exc) {
+                System.out.println(Exc);
+            }
+        }
+    }
+}
