8a1e790fb57a4eb3bc696027a847ad4a4a06923a
Enemo <furkan_ay96@hotmail.com>
2017-03-25 22:34:12 +0300

    .

--- software-lab-II-II/src/main/java/Initializer.java
+++ software-lab-II-II/src/main/java/Initializer.java
@@ -1,17 +1,54 @@
+import static util.Commons.toIntArray;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Initializer
 {
-    public static void main(String[] args)
+    private static final String FILE_NAME ="inputs/haberman.txt";
+    public static void main(String[] args) throws FileNotFoundException
     {
         new Initializer().run();
     }
 
-    void run()
+    void run() throws FileNotFoundException
     {
-        Thread_Read threadRead=new Thread_Read();
-        Thread_Write threadWrite=new Thread_Write();
-        threadRead.start();
-        threadWrite.start();
 
+       readAttributesFromFile(FILE_NAME);
+
+
+    }
+
+
+    private List<int[]> readAttributesFromFile(String fileName) throws FileNotFoundException
+
+    {
+        List<int[]> attributes = new ArrayList<>();
+        File file = new File(getFilePath(fileName));
+        Scanner read = new Scanner(file);
+
+        while (read.hasNextLine())
+        {
+            String[] attri = read.nextLine().split(",");
+            attributes.add(toIntArray(attri));
+
+        }
+        //faaa
+
+
+
+
+        return attributes;
+
+
+    }
+    private String getFilePath(String filename)
+    {
+        ClassLoader loader = getClass().getClassLoader();
+        return loader.getResource(filename).getFile();
     }
 }
 
