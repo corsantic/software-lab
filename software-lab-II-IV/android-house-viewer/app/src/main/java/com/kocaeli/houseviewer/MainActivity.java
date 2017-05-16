b830ce0e01f7f41f4018250defd46054cf25ea79
Enemo <furkan_ay96@hotmail.com>
2017-05-16 23:17:23 +0300

    getirmeyi yaptık

--- software-lab-II-IV/android-house-viewer/app/src/main/java/com/kocaeli/houseviewer/MainActivity.java
+++ software-lab-II-IV/android-house-viewer/app/src/main/java/com/kocaeli/houseviewer/MainActivity.java
@@ -1,15 +1,124 @@
 package com.kocaeli.houseviewer;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity
-{
+import com.loopj.android.http.HttpGet;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.protocol.BasicHttpContext;
+import cz.msebera.android.httpclient.protocol.HttpContext;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.my_button).setOnClickListener(this);
+    }
+
+
+    @Override
+
+
+    public void onClick(View arg0) {
+        Button b = (Button) findViewById(R.id.my_button);
+
+        b.setClickable(false);
+        new LongRunningGetIO().execute();
+    }
+
+    private class LongRunningGetIO extends AsyncTask<Void, Void, String> {
+
+        protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+            InputStream in = entity.getContent();
+
+
+            StringBuffer out = new StringBuffer();
+            int n = 1;
+            while (n > 0) {
+                byte[] b = new byte[4096];
+                n = in.read(b);
+
+
+                if (n > 0) out.append(new String(b, 0, n));
+            }
+
+
+            return out.toString();
+        }
+
+
+        @Override
+
+        protected String doInBackground(Void... params) {
+
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            HttpGet httpGet = new HttpGet("http://ferdielik.me:8081/");
+            String text = null;
+            try {
+                HttpResponse response = httpClient.execute(httpGet, localContext);
+
+
+                HttpEntity entity = response.getEntity();
+
+
+                text = getASCIIContentFromEntity(entity);
+
+
+            } catch (Exception e) {
+                return e.getLocalizedMessage();
+            }
+
+
+            return text;
+        }
+
+
+        protected void onPostExecute(String results) {
+
+            if (results != null) {
+
+
+                EditText et = (EditText) findViewById(R.id.my_edit);
+
+
+                et.setText(results);
+
+
+            }
+
+
+
+            Button b = (Button)findViewById(R.id.my_button);
+
+
+            b.setClickable(true);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
