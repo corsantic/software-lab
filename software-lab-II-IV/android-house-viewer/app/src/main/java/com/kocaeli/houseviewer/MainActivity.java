2f891e5fdc6cabd68329ccece675b23cb7079d6a
Enemo <furkan_ay96@hotmail.com>
2017-05-20 00:20:01 +0300

    .

--- software-lab-II-IV/android-house-viewer/app/src/main/java/com/kocaeli/houseviewer/MainActivity.java
+++ software-lab-II-IV/android-house-viewer/app/src/main/java/com/kocaeli/houseviewer/MainActivity.java
@@ -1,124 +1,132 @@
 package com.kocaeli.houseviewer;
 
-import android.os.AsyncTask;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-import com.loopj.android.http.HttpGet;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.kocaeli.houseviewer.entity.House;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import cz.msebera.android.httpclient.HttpEntity;
-import cz.msebera.android.httpclient.HttpResponse;
-import cz.msebera.android.httpclient.client.HttpClient;
-import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
-import cz.msebera.android.httpclient.protocol.BasicHttpContext;
-import cz.msebera.android.httpclient.protocol.HttpContext;
+public class MainActivity extends Activity {
+    Spinner sp;
+    int chosen;
+    TextView t1, t2, t3, t4;
+    ProgressDialog pDialog;
+    String veri_string;
+    public class AppConstants {
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+        public static final String URL = "http://ferdielik.me:8081";
 
 
+        /* private Constructor to avoid instanciating this class */
+        private AppConstants() {}
+    }
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.my_button).setOnClickListener(this);
-    }
 
+        // activity_main.xml de oluşturduğumuz textview leri koda tanıtıyoruz.
+        t1 = (TextView) findViewById(R.id.text);
+        t2 = (TextView) findViewById(R.id.text2);
+        t3 = (TextView) findViewById(R.id.text3);
+        t4 = (TextView) findViewById(R.id.text4);
 
-    @Override
+        // activity_main.xml de oluşturduğumuz spineri koda tanıtıyoruz.
+        sp = (Spinner) findViewById(R.id.spinner1);
 
+        // Spinera Listener ekliyoruz
+        sp.setOnItemSelectedListener(new OnItemSelectedListener() {
 
-    public void onClick(View arg0) {
-        Button b = (Button) findViewById(R.id.my_button);
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                       int arg2, long arg3) {
+                chosen = arg2; // spinerdan secilen değerin sonra değerini
+                // alıyoruz. kaçıncı sırada olduqunu
+                if (chosen != 0) { // seçilen deger ilk deger değilse yani Kişi
+                    // seçiniz yazısı değilse
+                    pDialog = new ProgressDialog(MainActivity.this);
+                    pDialog.setMessage("Kişi Bilgileri Getiriliyor...");
+                    pDialog.setIndeterminate(true);
+                    pDialog.setCancelable(false); // ProgressDialog u iptal
+                    // edilemez
+                    // hale getirdik.
+                    pDialog.show();
+                    try {
+                        getData();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
-        b.setClickable(false);
-        new LongRunningGetIO().execute();
-    }
-
-    private class LongRunningGetIO extends AsyncTask<Void, Void, String> {
-
-        protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
-            InputStream in = entity.getContent();
-
-
-            StringBuffer out = new StringBuffer();
-            int n = 1;
-            while (n > 0) {
-                byte[] b = new byte[4096];
-                n = in.read(b);
-
-
-                if (n > 0) out.append(new String(b, 0, n));
+                }
             }
 
-
-            return out.toString();
-        }
-
-
-        @Override
-
-        protected String doInBackground(Void... params) {
-
-
-            HttpClient httpClient = new DefaultHttpClient();
-            HttpContext localContext = new BasicHttpContext();
-            HttpGet httpGet = new HttpGet("http://ferdielik.me:8081/");
-            String text = null;
-            try {
-                HttpResponse response = httpClient.execute(httpGet, localContext);
-
-
-                HttpEntity entity = response.getEntity();
-
-
-                text = getASCIIContentFromEntity(entity);
-
-
-            } catch (Exception e) {
-                return e.getLocalizedMessage();
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {//OnItemSelectedListener implement methodu
             }
-
-
-            return text;
-        }
-
-
-        protected void onPostExecute(String results) {
-
-            if (results != null) {
-
-
-                EditText et = (EditText) findViewById(R.id.my_edit);
-
-
-                et.setText(results);
-
-
-            }
-
-
-
-            Button b = (Button)findViewById(R.id.my_button);
-
-
-            b.setClickable(true);
-        }
+        });
     }
+
+    public void getData() throws IOException {
+        // TODO adding request to POST method and URL
+        // Burada issteğimizi oluşturuyoruz, method parametresi olarak post
+        // seçiyoruz ve url'imizi const'dan alıyoruz.
+
+
+        StringRequest myReq = new StringRequest(Request.Method.GET, AppConstants.URL, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+
+                Gson gson = new Gson();
+                Type listType = new TypeToken<ArrayList<House>>(){}.getType();
+
+                List<House> houses = gson.fromJson(response, listType); //PersonClass'mızdan obje oluşturuyoruz böylelikle set ettiğimiz verilere ulaşacağız.
+                pDialog.dismiss(); // ProgresDialog u kapatıyoruz.
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error.getMessage());
+            }
+        }) {
+
+            protected Map<String, String> getParams()
+                    throws com.android.volley.AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                // TODO seçilen kişinin id'sini kisi parametresine ekliyoruz
+                params.put("kisi", chosen + "");
+                return params;
+            };
+        };
+        myReq.setShouldCache(false); //cache kapatıyoruz
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request(İstek)'i Volley'in Requst sırasına atıyoruz
+        // Adding request to volley request queue
+        queue.add(myReq);
+
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
